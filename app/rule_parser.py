f56ad554f46882abaecdeeeb47287e9906439816
Lorenzo Lasagni <l.lasagni01@teamsystem.com>
2023-09-21 00:06:09 +0200

    feat(parser): add parser for rule
    Created parser to convert string rules into tree of statement and operations

---
+++ app/rule_parser.py
@@ -0,0 +1,44 @@
+from app.statement_parser import parse_statement
+
+
+def parse_rule(rule):
+    statementList = rule.split("\n")
+    statementList[:] = [x.strip() for x in statementList if x]
+
+    operator_stack = []
+    operand_stack = []
+    for statement in statementList:
+        if statement not in ['AND', 'OR', '(', ')']:
+            operand_stack.append(parse_statement(statement))
+        elif statement == 'OR' and len(operator_stack) > 0 and operator_stack[-1] == 'AND':
+            right_operand, left_operand = operand_stack.pop(), operand_stack.pop()
+            operator = operator_stack.pop()
+            operand_stack.append({
+                "operator": operator,
+                "right_operand": right_operand,
+                "left_operand": left_operand
+            })
+            operator_stack.append(statement)
+        elif statement == ')':
+            while len(operator_stack) > 0 and operator_stack[-1] != '(':
+                right_operand, left_operand = operand_stack.pop(), operand_stack.pop()
+                operator = operator_stack.pop()
+                operand_stack.append({
+                    "operator": operator,
+                    "right_operand": right_operand,
+                    "left_operand": left_operand
+                })
+            operator_stack.pop()
+        else:
+            operator_stack.append(statement)
+
+    while len(operator_stack) > 0:
+        right_operand, left_operand = operand_stack.pop(), operand_stack.pop()
+        operator = operator_stack.pop()
+        operand_stack.append({
+            "operator": operator,
+            "right_operand": right_operand,
+            "left_operand": left_operand
+        })
+
+    return operand_stack.pop()
---
+++ tests/test_rule_parser.py
@@ -0,0 +1,137 @@
+
+import unittest
+from unittest.mock import Mock, patch
+
+
+class TestRuleParser(unittest.TestCase):
+    def setUp(self) -> None:
+        statement_parser = Mock(
+            **{'parse_statement.side_effect': lambda value: value})
+
+        modules = {'app.statement_parser': statement_parser}
+        patch.dict('sys.modules', modules).start()
+
+        from app.rule_parser import parse_rule
+        self.parse_rule = parse_rule
+
+        return super().setUp()
+
+    def test_simple_and_rule(self):
+        rule = """\
+score is above 50
+AND
+score is below 100
+"""
+        expected = {
+            "operator": "AND",
+            "left_operand": "score is above 50",
+            "right_operand": "score is below 100"
+        }
+
+        self.assertEqual(expected, self.parse_rule(rule))
+
+    def test_simple_or_rule(self):
+        rule = """\
+score is below 50
+OR
+score is above 100
+"""
+        expected = {
+            "operator": "OR",
+            "left_operand": "score is below 50",
+            "right_operand": "score is above 100"
+        }
+
+        self.assertEqual(expected, self.parse_rule(rule))
+
+    def test_complex_and_or_rule(self):
+        rule = """\
+score is below 50
+AND
+score is above 100
+OR
+score is equal 0
+"""
+        expected = {
+            "operator": "OR",
+            "right_operand": "score is equal 0",
+            "left_operand": {
+                "operator": "AND",
+                "left_operand": "score is below 50",
+                "right_operand": "score is above 100"
+            }
+        }
+
+        self.assertEqual(expected, self.parse_rule(rule))
+
+    def test_rule_with_parentheses(self):
+        rule = """\
+(
+    score is below 50
+    OR
+    score is above 100
+)
+AND
+(
+    risk is below 10
+    OR
+    value is below 1000
+)
+
+"""
+        expected = {
+            "operator": "AND",
+            "left_operand": {
+                "operator": "OR",
+                "left_operand": "score is below 50",
+                "right_operand": "score is above 100"
+            },
+            "right_operand": {
+                "operator": "OR",
+                "left_operand": "risk is below 10",
+                "right_operand": "value is below 1000"
+            }
+        }
+
+        self.assertEqual(expected, self.parse_rule(rule))
+
+    def test_complex_rule_with_parentheses(self):
+        rule = """\
+(
+    score is below 50
+    OR
+    score is above 100
+)
+AND
+(
+    risk is below 10
+    OR
+    value is below 1000
+    OR
+    credit_rating is above 23
+)
+
+"""
+        expected = {
+            "operator": "AND",
+            "left_operand": {
+                "operator": "OR",
+                "left_operand": "score is below 50",
+                "right_operand": "score is above 100"
+            },
+            "right_operand": {
+                "operator": "OR",
+                "left_operand": "risk is below 10",
+                "right_operand": {
+                    "operator": "OR",
+                    "left_operand": "value is below 1000",
+                    "right_operand": "credit_rating is above 23",
+                }
+            }
+        }
+
+        self.assertEqual(expected, self.parse_rule(rule))
+
+
+if __name__ == '__main__':
+    unittest.main()
